c7d7993e93dd3afc219a68745b3fef0e8abe28c8
kamiwey <kamiwey@gmail.com>
2022-12-01 15:05:00 +0000

    almost done

---
+++ src/front/js/pages/privated.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect, useContext } from "react";
+import { Link, Navigate, useNavigate} from "react-router-dom";
+
+
+import { Context } from "../store/appContext";
+
+export const Privated = () => {
+    const {store, actions} = useContext(Context)
+    const navigate = useNavigate();
+    const token = localStorage.getItem('token');
+    
+   useEffect(() =>{
+   actions.private()}
+   ,[store.auth])
+
+    return (
+            <>
+                {token && store.auth === true? 
+                    <div className="text-center"> 
+                        <h1>Página personal que ha realizado la autentificacion jwt_require</h1>
+                        {/* <Link className="btn btn-primary" to="/privated">Home</Link>  */}
+                    
+                          <Link to="/" type="submit" className="btn btn-primary" onClick={() => {
+                            actions.logout();
+                         }}>
+                         Logout
+                         </Link>
+                       </div>
+                : navigate("/") 
+                }
+            </>
+        )
+
+};
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,54 +1,126 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
-		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
-				}
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
+	  store: {
+		message: null,
+		token: "",
+		auth: false,
+	  },
+	  actions: {
+		// Use getActions to call a function within a fuction
+  
+		signup: async (infouserpassw) => {
+		  await fetch(
+			"https://3001-kamiwey-40jwtauthentica-ui3amu810w2.ws-eu77.gitpod.io/signup",
+			{
+			  method: "POST",
+			  body: JSON.stringify(infouserpassw),
+			  headers: { "Content-Type": "application/json" },
 			}
-		}
+		  ).then((resp) => {
+			if (resp.ok) {
+			  console.log("registro OK");
+			}
+		  });
+		},
+  
+		login: (infouserpass) => {
+		  const response = fetch(
+			"https://3001-kamiwey-40jwtauthentica-ui3amu810w2.ws-eu77.gitpod.io/token",
+			{
+			  //mode: 'no-cors',
+			  method: "POST",
+			  body: JSON.stringify(infouserpass),
+			  headers: { "Content-Type": "application/json" },
+			}
+		  )
+		  .then(function(response) {
+			if (!response.ok) {
+			throw Error(response.statusText);
+			}
+			else{
+			  setStore({ auth: true });
+			  const { auth } = getStore();
+				  console.log("auth1", auth)
+			  
+			}
+			return response.json()
+			// Aquí es donde pones lo que quieres hacer con la respuesta.
+		})
+		.then(data =>{localStorage.setItem("token", data.token); })
+		.catch();
+		},
+  
+		private: async () => {
+		  let tok = localStorage.getItem("token");
+		  
+		  //if (tok == getStore().token) {
+			await fetch(
+			  "https://3001-kamiwey-40jwtauthentica-ui3amu810w2.ws-eu77.gitpod.io/privated",
+			  {
+				method: "GET",
+				headers: {
+				  "Content-Type": "application/json",
+				  Authorization: "Bearer " + tok,
+				},
+			  }
+			).then((res) => {
+			  if (res.status == 200) {
+				console.log("Todo bien con el fetch en private");
+				const { auth } = getStore();
+				console.log("auth4", auth)
+				setStore({ auth: true });
+			  } else {
+				console.log(
+				  "Algo ha ido mal con el token y el require en el private Fetch"
+				);
+				// return res.json()
+			  }
+			});
+		  // } else {
+		  //   return "Validation error flux 97";
+		  // }
+		},
+  
+		exampleFunction: () => {
+		  getActions().changeColor(0, "green");
+		},
+  
+		logout: ()=>{
+	  const { auth } = getStore();
+		  localStorage.removeItem("token")
+		  setStore({auth: false})
+	  console.log("auth3", auth)
+		},
+  
+		getMessage: async () => {
+		  try {
+			// fetching data from the backend
+			const resp = await fetch(process.env.BACKEND_URL + "/hello");
+			const data = await resp.json();
+			setStore({ message: data.message });
+			// don't forget to return something, that is how the async resolves
+			return data;
+		  } catch (error) {
+			console.log("Error loading message from backend", error);
+		  }
+		},
+		changeColor: (index, color) => {
+		  //get the store
+		  const store = getStore();
+  
+		  //we have to loop the entire demo array to look for the respective index
+		  //and change its color
+		  const demo = store.demo.map((elm, i) => {
+			if (i === index) elm.background = color;
+			return elm;
+		  });
+  
+		  //reset the global store
+		  setStore({ demo: demo });
+		},
+	  },
 	};
-};
-
-export default getState;
+  };
+  
+  export default getState;
+  
